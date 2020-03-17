5ff0c79b3366ca1f126472c2e48e676dc1297e04
F-G Fernandez <fgfm03@hotmail.fr>
2020-03-17 02:17:26 +0100

    fix: Fixed no GPU cases
    If the nvidia-smi command fails, it will throw an error. Instead catch the error and throw a warning assuming the system is CPU only.

--- torchscan/process/memory.py
+++ torchscan/process/memory.py
@@ -7,6 +7,7 @@ Process memory
 
 import re
 import subprocess
+import warnings
 
 __all__ = ['get_process_gpu_ram']
 
@@ -21,12 +22,15 @@ def get_process_gpu_ram(pid):
     """
 
     # Query the running processes on GPUs
-    res = subprocess.run(["nvidia-smi", "-q", "-d", "PIDS"], capture_output=True).stdout.decode()
-    # Try to locate the process
-    pids = re.findall("Process ID\s+:\s([^\D]*)", res)
-    for idx, _pid in enumerate(pids):
-        if int(_pid) == pid:
-            return float(re.findall("Used GPU Memory\s+:\s([^\D]*)", res)[idx])
+    try:
+        res = subprocess.run(["nvidia-smi", "-q", "-d", "PIDS"], capture_output=True).stdout.decode()
+        # Try to locate the process
+        pids = re.findall("Process ID\s+:\s([^\D]*)", res)
+        for idx, _pid in enumerate(pids):
+            if int(_pid) == pid:
+                return float(re.findall("Used GPU Memory\s+:\s([^\D]*)", res)[idx])
+    except Exception as e:
+        warnings.warn(UserWarning, 'unable to retrieve nvidia-smi output. Assuming no GPU is available.')
 
     # Otherwise assume the process is running exclusively on CPU
     return 0.

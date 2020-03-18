8e8ad8f1a3527f4a6c346de51fbecf1364557462
F-G Fernandez <fgfm03@hotmail.fr>
2020-03-19 00:53:47 +0100

    test: Updated test for process

--- test/test_process.py
+++ test/test_process.py
@@ -1,12 +1,17 @@
 import unittest
 import os
 from torchscan import process
+import torch
 
 
 class Tester(unittest.TestCase):
 
     def test_get_process_gpu_ram(self):
-        self.assertEqual(process.get_process_gpu_ram(os.getpid()), 0)
+
+        if torch.cuda.is_initialized:
+            self.assertGreater(process.get_process_gpu_ram(os.getpid()), 0)
+        else:
+            self.assertEqual(process.get_process_gpu_ram(os.getpid()), 0)
 
 
 if __name__ == '__main__':

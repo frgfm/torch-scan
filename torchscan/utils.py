782e8c95157152adc71afc79a686d74c5510a6bd
F-G Fernandez <fgfm03@hotmail.fr>
2021-05-08 20:20:58 +0200

    chore: Updated license from MIT to Apache2.0 (#44)
    * chore: Updated license
    
    * docs: Updated README
    
    * docs: Updated copyrght and license notices

--- torchscan/utils.py
+++ torchscan/utils.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2020-2021, François-Guillaume Fernandez.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
 from typing import Tuple, Dict, Any, Optional, List
 
 

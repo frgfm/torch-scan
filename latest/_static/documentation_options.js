1effc1465e97f7fa3075d0b7a84f94fae9e22771
frgfm <fgfm03@hotmail.fr>
2021-07-11 17:32:16 +0000

    [skip ci] Documentation updates

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1+de4472e-git',
+    VERSION: '0.1.1+61116d9-git',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

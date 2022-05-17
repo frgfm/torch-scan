fix: Fixed MACs/FLOPs computation for Linear

torchscan/modules/macs.py
@@ -5,6 +5,7 @@
 
 import warnings
 from functools import reduce
+from math import prod
 from operator import mul
 
 from torch import nn
@@ -57,7 +58,7 @@ def macs_linear(module: nn.Linear, input: Tensor, output: Tensor) -> int:
     """MACs estimation for `torch.nn.Linear`"""
 
     # batch size * out_chan * macs_per_elt (bias already counted in accumulation)
-    mm_mac = input.shape[0] * output.shape[1] * input.shape[1]
+    mm_mac = module.in_features * prod(output.shape)
 
     return mm_mac
 
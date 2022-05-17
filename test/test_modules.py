fix: Fixed MAC/FLOP computation for Linear in higher dimensions (#52)
* test: Extended unittests for Linear

* fix: Fixed MACs/FLOPs computation for Linear

* docs: Fixed crawler docstring

* fix: Fixed compat with Python <3.8

* fix: Fixed flops computation

* fix: Fixed FLOPs for Linear

* test: Speeded up transformer scan test

* test: Fixed sizing of unittest input

* test: Updated expected result of unittest

test/test_modules.py
@@ -29,6 +29,8 @@ class Tester(unittest.TestCase):
                          4 * (2 * 8 - 1) + 4)
         self.assertEqual(modules.module_flops(nn.Linear(8, 4, bias=False), (torch.zeros((1, 8)),), torch.zeros((1, 4))),
                          4 * (2 * 8 - 1))
+        self.assertEqual(modules.module_flops(nn.Linear(8, 4), (torch.zeros((1, 2, 8)),), torch.zeros((1, 2, 4))),
+                         2 * (4 * (2 * 8 - 1) + 4))
         # Activations
         self.assertEqual(modules.module_flops(nn.Identity(), (torch.zeros((1, 8)),), torch.zeros((1, 8))), 0)
         self.assertEqual(modules.module_flops(nn.Flatten(), (torch.zeros((1, 8)),), torch.zeros((1, 8))), 0)
@@ -77,10 +79,10 @@ class Tester(unittest.TestCase):
         mod = nn.ConvTranspose2d(3, 8, 3)
         self.assertEqual(modules.module_flops(mod, (input_t,), mod(input_t)), 499408)
         # Transformer
-        mod = nn.Transformer(nhead=4, num_encoder_layers=3)
-        src = torch.rand((10, 32, 512))
-        tgt = torch.rand((20, 32, 512))
-        self.assertEqual(modules.module_flops(mod, (src, tgt), mod(src, tgt)), 1916295945)
+        mod = nn.Transformer(d_model=64, nhead=4, num_encoder_layers=3)
+        src = torch.rand((10, 16, 64))
+        tgt = torch.rand((20, 16, 64))
+        self.assertEqual(modules.module_flops(mod, (src, tgt), mod(src, tgt)), 774952841)
 
     @torch.no_grad()
     def test_module_macs(self):
@@ -92,6 +94,8 @@ class Tester(unittest.TestCase):
         # Linear
         self.assertEqual(modules.module_macs(nn.Linear(8, 4), torch.zeros((1, 8)), torch.zeros((1, 4))),
                          8 * 4)
+        self.assertEqual(modules.module_macs(nn.Linear(8, 4), torch.zeros((1, 2, 8)), torch.zeros((1, 2, 4))),
+                         8 * 4 * 2)
         # Activations
         self.assertEqual(modules.module_macs(nn.ReLU(), None, None), 0)
         # Conv

torchscan/modules/flops.py
@@ -74,8 +74,9 @@ def flops_linear(module: nn.Linear, inputs: Tuple[Tensor, ...]) -> int:
     """FLOPs estimation for `torch.nn.Linear`"""
 
     # batch size * out_chan * in_chan
-    mm_flops = inputs[0].shape[0] * module.out_features * (2 * module.in_features - 1)
-    bias_flops = inputs[0].shape[0] * module.out_features if module.bias is not None else 0
+    num_out_feats = module.out_features * reduce(mul, inputs[0].shape[:-1])
+    mm_flops = num_out_feats * (2 * module.in_features - 1)
+    bias_flops = num_out_feats if module.bias is not None else 0
 
     return mm_flops + bias_flops
 

torchscan/modules/macs.py
@@ -57,7 +57,7 @@ def macs_linear(module: nn.Linear, input: Tensor, output: Tensor) -> int:
     """MACs estimation for `torch.nn.Linear`"""
 
     # batch size * out_chan * macs_per_elt (bias already counted in accumulation)
-    mm_mac = input.shape[0] * output.shape[1] * input.shape[1]
+    mm_mac = module.in_features * reduce(mul, output.shape)
 
     return mm_mac
 
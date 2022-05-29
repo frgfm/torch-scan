0bda422aae038be7026cf35f126f813a1ad5bbfd
frgfm <26927750+frgfm@users.noreply.github.com>
2022-05-29 19:24:56 +0200

    refactor: Updated requirement validation script

--- .github/validate_deps.py
+++ .github/validate_deps.py
@@ -3,60 +3,88 @@ from pathlib import Path
 import requirements
 from requirements.requirement import Requirement
 
-# Deps that won't have a specific requirements.txt
-IGNORE = ["flake8", "isort", "mypy", "pydocstyle"]
 # All req files to check
-REQ_FILES = ["requirements.txt", "tests/requirements.txt", "docs/requirements.txt"]
+EXTRA_MAP = {
+    "test": "tests/requirements.txt",
+    "docs": "docs/requirements.txt",
+}
+EXTRA_IGNORE = ["dev"]
 
 
-def main():
+def parse_deps(deps):
+    reqs = {}
+    for _dep in deps:
+        lib, specs = _dep
+        assert reqs.get(lib) is None, f"conflicting deps for {lib}"
+        reqs[lib] = specs
 
-    # Collect the deps from all requirements.txt
+    return reqs
+
+
+def get_conficts(setup_reqs, requirement_file):
+
+    # Parse the deps from the requirements.txt
     folder = Path(__file__).parent.parent.absolute()
     req_deps = {}
-    for file in REQ_FILES:
-        with open(folder.joinpath(file), 'r') as f:
-            _deps = [(req.name, req.specs) for req in requirements.parse(f)]
+    with open(folder.joinpath(requirement_file), 'r') as f:
+        _deps = [(req.name, req.specs) for req in requirements.parse(f)]
 
-        for _dep in _deps:
-            lib, specs = _dep
-            assert req_deps.get(lib, specs) == specs, f"conflicting deps for {lib}"
-            req_deps[lib] = specs
-
-    # Collect the one from setup.py
-    setup_deps = {}
-    with open(folder.joinpath("setup.py"), 'r') as f:
-        setup = f.readlines()
-    lines = setup[setup.index("_deps = [\n") + 1:]
-    lines = [_dep.strip() for _dep in lines[:lines.index("]\n")]]
-    lines = [_dep.split('"')[1] for _dep in lines if _dep.startswith('"')]
-    _reqs = [Requirement.parse(_line) for _line in lines]
-    _deps = [(req.name, req.specs) for req in _reqs]
-    for _dep in _deps:
-        lib, specs = _dep
-        assert setup_deps.get(lib) is None, f"conflicting deps for {lib}"
-        setup_deps[lib] = specs
-
-    # Remove ignores
-    for k in IGNORE:
-        if isinstance(req_deps.get(k), list):
-            del req_deps[k]
-        if isinstance(setup_deps.get(k), list):
-            del setup_deps[k]
+    req_deps = parse_deps(_deps)
 
     # Compare them
-    assert len(req_deps) == len(setup_deps)
+    assert len(req_deps) == len(setup_reqs)
     mismatches = []
-    for k, v in setup_deps.items():
+    for k, v in setup_reqs.items():
         assert isinstance(req_deps.get(k), list)
         if req_deps[k] != v:
             mismatches.append((k, v, req_deps[k]))
 
-    if len(mismatches) > 0:
+    return mismatches
+
+
+def main():
+
+    # Collect the one from setup.py
+    folder = Path(__file__).parent.parent.absolute()
+    with open(folder.joinpath("setup.cfg"), 'r') as f:
+        setup = f.readlines()
+
+    # install_requires
+    lines = setup[setup.index("install_requires =\n") + 1:]
+    lines = [_dep.strip() for _dep in lines[:lines.index("\n")]]
+    _reqs = [Requirement.parse(_line) for _line in lines]
+    install_requires = parse_deps([(req.name, req.specs) for req in _reqs])
+
+    # extras
+    extras_require = {}
+    lines = setup[setup.index("[options.extras_require]\n") + 1:]
+    lines = lines[:lines.index("\n")]
+    # Split each extra
+    extra_lines = [_line for _line in lines if str.isalpha(_line[0])]
+    extra_names = [_line.strip().replace("=", "").strip() for _line in extra_lines]
+    for current_extra, start_line, end_line in zip(extra_names, extra_lines, extra_lines[1:] + [None]):
+        if current_extra in EXTRA_IGNORE:
+            continue
+        _lines = [_dep for _dep in lines[lines.index(start_line) + 1:]]
+        if isinstance(end_line, str):
+            _lines = _lines[:_lines.index(end_line)]
+        # Remove comments
+        _lines = [_line.strip() for _line in _lines]
+        _reqs = [Requirement.parse(_line.strip()) for _line in _lines if not _line.strip().startswith("#")]
+        extras_require[current_extra] = parse_deps([(req.name, req.specs) for req in _reqs])
+
+    # Resolve conflicts
+    mismatches = {}
+    mismatches["requirements.txt"] = get_conficts(install_requires, "requirements.txt")
+    for extra_k, req_file in EXTRA_MAP.items():
+        mismatches[req_file] = get_conficts(extras_require[extra_k], req_file)
+
+    # Display the results
+    if any(len(mismatch) > 0 for mismatch in mismatches.values()):
         mismatch_str = "version specifiers mismatches:\n"
         mismatch_str += '\n'.join(
-            f"- {lib}: {setup} (from setup.py) | {reqs} (from requirements)"
-            for lib, setup, reqs in mismatches
+            f"- {lib}: {setup} (from setup.cfg) | {reqs} (from {req_file})"
+            for req_file, issues in mismatches.items() for lib, setup, reqs in issues
         )
         raise AssertionError(mismatch_str)
 

6208fa36069bd7063d617b7bb54b33163b24c171
frgfm <26927750+frgfm@users.noreply.github.com>
2022-05-31 04:44:13 +0200

    ci: Updated dependency check

--- .github/validate_deps.py
+++ .github/validate_deps.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import requirements
+import toml
 from requirements.requirement import Requirement
 
 # All req files to check
@@ -8,10 +9,9 @@ EXTRA_MAP = {
     "test": "tests/requirements.txt",
     "docs": "docs/requirements.txt",
 }
-EXTRA_IGNORE = ["dev"]
 
 
-def parse_deps(deps):
+def check_deps(deps):
     reqs = {}
     for _dep in deps:
         lib, specs = _dep
@@ -26,10 +26,10 @@ def get_conficts(setup_reqs, requirement_file):
     # Parse the deps from the requirements.txt
     folder = Path(__file__).parent.parent.absolute()
     req_deps = {}
-    with open(folder.joinpath(requirement_file), 'r') as f:
+    with open(folder.joinpath(requirement_file), "r") as f:
         _deps = [(req.name, req.specs) for req in requirements.parse(f)]
 
-    req_deps = parse_deps(_deps)
+    req_deps = check_deps(_deps)
 
     # Compare them
     assert len(req_deps) == len(setup_reqs)
@@ -46,32 +46,18 @@ def main():
 
     # Collect the one from setup.py
     folder = Path(__file__).parent.parent.absolute()
-    with open(folder.joinpath("setup.cfg"), 'r') as f:
-        setup = f.readlines()
+    toml_reqs = toml.load(folder.joinpath("pyproject.toml"))
 
     # install_requires
-    lines = setup[setup.index("install_requires =\n") + 1:]
-    lines = [_dep.strip() for _dep in lines[:lines.index("\n")]]
-    _reqs = [Requirement.parse(_line) for _line in lines]
-    install_requires = parse_deps([(req.name, req.specs) for req in _reqs])
+    _reqs = [Requirement.parse(_line) for _line in toml_reqs["project"]["dependencies"]]
+    install_requires = check_deps([(req.name, req.specs) for req in _reqs])
 
     # extras
-    extras_require = {}
-    lines = setup[setup.index("[options.extras_require]\n") + 1:]
-    lines = lines[:lines.index("\n")]
-    # Split each extra
-    extra_lines = [_line for _line in lines if str.isalpha(_line[0])]
-    extra_names = [_line.strip().replace("=", "").strip() for _line in extra_lines]
-    for current_extra, start_line, end_line in zip(extra_names, extra_lines, extra_lines[1:] + [None]):
-        if current_extra in EXTRA_IGNORE:
-            continue
-        _lines = [_dep for _dep in lines[lines.index(start_line) + 1:]]
-        if isinstance(end_line, str):
-            _lines = _lines[:_lines.index(end_line)]
-        # Remove comments
-        _lines = [_line.strip() for _line in _lines]
-        _reqs = [Requirement.parse(_line.strip()) for _line in _lines if not _line.strip().startswith("#")]
-        extras_require[current_extra] = parse_deps([(req.name, req.specs) for req in _reqs])
+    extras_require = {
+        k: [Requirement.parse(_line) for _line in lines]
+        for k, lines in toml_reqs["project"]["optional-dependencies"].items()
+    }
+    extras_require = {k: check_deps([(req.name, req.specs) for req in _reqs]) for k, _reqs in extras_require.items()}
 
     # Resolve conflicts
     mismatches = {}
@@ -82,11 +68,13 @@ def main():
     # Display the results
     if any(len(mismatch) > 0 for mismatch in mismatches.values()):
         mismatch_str = "version specifiers mismatches:\n"
-        mismatch_str += '\n'.join(
+        mismatch_str += "\n".join(
             f"- {lib}: {setup} (from setup.cfg) | {reqs} (from {req_file})"
-            for req_file, issues in mismatches.items() for lib, setup, reqs in issues
+            for req_file, issues in mismatches.items()
+            for lib, setup, reqs in issues
         )
         raise AssertionError(mismatch_str)
+
 
 if __name__ == "__main__":
     main()
